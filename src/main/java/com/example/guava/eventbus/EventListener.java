1cf999d242e80a6cac8f291655ddb5b2cd988fe8
Chun <caochun@gmail.com>
2020-04-24 23:45:02 +0800

    first commit

---
+++ src/main/java/com/example/guava/eventbus/EventListener.java
@@ -0,0 +1,39 @@
+package com.example.guava.eventbus;
+
+import com.google.common.eventbus.DeadEvent;
+import com.google.common.eventbus.Subscribe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventListener {
+
+    private static int eventsHandled;
+    private static final Logger LOG = LoggerFactory.getLogger(EventListener.class);
+
+    @Subscribe
+    public void stringEvent(String event) {
+        LOG.info("get string event [" + event + "]");
+        eventsHandled++;
+    }
+
+    @Subscribe
+    public void someCustomEvent(CustomEvent customEvent) {
+        LOG.info("get custom event [" + customEvent.getAction() + "]");
+        eventsHandled++;
+    }
+
+    @Subscribe
+    public void handleDeadEvent(DeadEvent deadEvent) {
+        LOG.info("unhandled event [" + deadEvent.getEvent() + "]");
+        eventsHandled++;
+    }
+
+    int getEventsHandled() {
+        return eventsHandled;
+    }
+
+    void resetEventsHandled() {
+        eventsHandled = 0;
+    }
+}
